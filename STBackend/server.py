fb7d42f37f5d6da8885844f671389d1369047e16
jenniferli8263 <75101197+jenniferli8263@users.noreply.github.com>
2025-07-06 16:53:24 -0400

    signup and login

--- STBackend/server.py
+++ STBackend/server.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from db import init_db, close_db
-from routes import listings, hello, renters
+from routes import listings, hello, renters, auth
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -14,6 +14,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(listings.router)
+app.include_router(auth.router)
 app.include_router(hello.router)
 app.include_router(renters.router)
 
