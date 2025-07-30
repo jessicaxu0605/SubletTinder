33a54ad1fc321050c5d2a563b5498c38fd26707d
jessicaxu0605 <jessicaxu0605@gmail.com>
2025-07-29 20:24:17 -0400

    R11 sql query

--- STBackend/database_setup/SQLQueries/test-production.sql
+++ STBackend/database_setup/SQLQueries/test-production.sql
@@ -78,6 +78,9 @@ SELECT id, email, password, first_name, last_name, profile_photo FROM users WHER
 -- Feature 5: Delete user
 DELETE from users WHERE id = 1001 RETURNING *;
 
+-- Advanced Feature 1: Listing Recommendation Shortlisting
+SELECT * FROM get_listing_candidates(3);
+
 -- Advanced Feature 5: Collaborative filtering
 WITH
   current_likes AS (
